3e3579e7d0b0100ba549792aa2ca70c8b15f4199
badiwidya <adiwidya.berton13@gmail.com>
2024-12-07 10:19:24 +0000

    Add several functions and write some logic And also some changes.

--- script.js
+++ script.js
@@ -1,48 +1,69 @@
 /* DOM Variable */
 
-const clear = document.querySelector("#clear");
+const clearEntry = document.querySelector("#clear");
 const display = document.querySelector("#display");
-const clearAll = document.querySelector("#clearall");
+const clear = document.querySelector("#clearall");
 const inputArea = document.querySelector("#input-buttons");
 
 /* Global Varibale */
 let expression = {
-  currentInput: '',
+  currentInput: "",
   operator: null,
   previousInput: null,
-}
+};
 
 /* Event Listener */
-clear.addEventListener("click", clearDisplay);
-clearAll.addEventListener("click", clearAll);
+clearEntry.addEventListener("click", clearDisplay);
+clear.addEventListener("click", clearAll);
 inputArea.addEventListener("click", (e) => {
   mainCalculator(e);
-})
+});
 
 /* Function */
-function mainCalculator(e, exp = expression) {
+function mainCalculator(e) {
   const userInput = getUserInput(e);
-  if (userInput.class.contains('operand')) {
-
-  } else if (userInput.class.contains('operator')) {
-    switch (userInput.id) {
-      
+  if (userInput.class.contains("operand")) {
+    if (expression.currentInput.length < 8) {
+      expression.currentInput += userInput.id;
+      setDisplay(expression.currentInput);
     }
+  } else if (userInput.class.contains("operator")) {
+
+    //? So the function works like, if you have a number in memory, operate it first.
+    //? If not, set the operator and put currentInput to the previousInput.
+
+    if (expression.previousInput && expression.operator) {
+      const result = operate(expression.previousInput, expression.currentInput, expression.operator);
+      expression.previousInput = null;
+      expression.currentInput = result;
+      setDisplay(expression.currentInput);
+    }
+    expression.operator = userInput.id;
+    expression.previousInput = expression.currentInput;
+    expression.currentInput = "";
+  } else if (userInput.class.contains("calculate")) {
   }
 }
 
+function operatorProcess(operator, exp = expression) {}
+
 function setDisplay(exp) {
+  if (exp.length > 8) {
+    display.textContent = parseFloat(exp).toExponential(3);
+    return;
+  }
   display.textContent = exp;
 }
 
 function clearDisplay() {
+  expression.currentInput = "";
   display.textContent = "0";
 }
 
-function clearAll(obj) {
-  obj.currentInput = '';
-  obj.operator = null;
-  obj.previousInput = null;
+function clearAll() {
+  expression.currentInput = "";
+  expression.operator = null;
+  expression.previousInput = null;
   clearDisplay();
 }
 // use .contains(args)
@@ -50,21 +71,23 @@ function getUserInput(e) {
   const inputDetail = {
     class: e.target.classList,
     id: e.target.id,
-  }
+  };
   return inputDetail;
 }
 
-function operate(a, b, operation) {
+function operate(num1, num2, operation) {
+  const a = Number(num1);
+  const b = Number(num2);
   switch (operation) {
-    case '+':
+    case "plus":
       return add(a, b);
-    case '-':
+    case "minus":
       return subtract(a, b);
-    case 'multiply':
+    case "multiply":
       return multiply(a, b);
-    case 'divide':
+    case "divide":
       return divide(a, b);
-    case 'modulus':
+    case "modulus":
       return modulus(a, b);
   }
 }
@@ -85,7 +108,7 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-  const result = a/b;
+  const result = a / b;
   return formatNumber(result);
 }
 
