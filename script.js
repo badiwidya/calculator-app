98fbfb82175c6af2436dd7c4942f2a00ae2edd19
badiwidya <adiwidya.berton13@gmail.com>
2024-12-07 07:24:17 +0000

    Create clearDisplay function for CE button

---
+++ script.js
@@ -0,0 +1,10 @@
+const clear = document.querySelector("#clear");
+const display = document.querySelector("#display");
+
+/* Event Listener */
+clear.addEventListener("click", clearDisplay);
+
+/* Function */
+function clearDisplay() {
+  display.textContent = "";
+}
