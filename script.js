93b8a4b3050e83804bcd839991d1554d49ef5815
badiwidya <adiwidya.berton13@gmail.com>
2024-12-07 09:00:12 +0000

    Fill each operator function

--- script.js
+++ script.js
@@ -31,20 +31,37 @@ function getUserInput(e) {
   return e.target.id;
 }
 
-function operate(operation) {
+function operate(a, b, operation) {
+  switch (operation) {
+    case '+':
+      return add(a, b);
+    case '-':
+      return subtract(a, b);
+    case '*':
+      return multiply(a, b);
+    case '/':
+      return divide(a, b);
+    case '%':
+      return modulus(a, b);
+  }
 }
 
 function add(a, b) {
+  return a+b;
 }
 
 function subtract(a, b) {
+  return a-b;
 }
 
 function multiply(a, b) {
+  return a*b;
 }
 
 function divide(a, b) {
+  return a/b;
 }
 
 function modulus(a, b) {
+  return a%b;
 }
