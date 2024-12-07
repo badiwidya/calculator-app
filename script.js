0311b387e17871aa677e61b1a6243f713fef6270
badiwidya <adiwidya.berton13@gmail.com>
2024-12-07 08:44:50 +0000

    Add clearall function

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 const clear = document.querySelector("#clear");
 const display = document.querySelector("#display");
 
+let expression = {
+  currentInput: '',
+  operator: null,
+  previousInput: null,
+}
+
 /* Event Listener */
 clear.addEventListener("click", clearDisplay);
 
 /* Function */
 function clearDisplay() {
   display.textContent = "0";
+}
+
+function clearAll(obj) {
+  obj.currentInput = '';
+  obj.operator = null;
+  obj.previousInput = null;
 }
 
 function getUserInput(e) {
