ee0d18a63cfbe2f479c56a8cf7f4206200a7037e
badiwidya <adiwidya.berton13@gmail.com>
2024-12-07 08:18:49 +0000

    Add some class to the button operand

--- script.js
+++ script.js
@@ -8,3 +8,25 @@ clear.addEventListener("click", clearDisplay);
 function clearDisplay() {
   display.textContent = "0";
 }
+
+function getUserInput(e) {
+  return e.target.id;
+}
+
+function operate(operation) {
+}
+
+function add(a, b) {
+}
+
+function subtract(a, b) {
+}
+
+function multiply(a, b) {
+}
+
+function divide(a, b) {
+}
+
+function modulus(a, b) {
+}
